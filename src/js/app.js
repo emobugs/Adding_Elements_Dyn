05f6089bf25ded62dd89da74b45d125fd3774fb2
Emo <emotupev11@gmail.com>
2023-01-27 19:58:25 +0200

    feat: add user interaction

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,14 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  document.body.addEventListener("click", () => {
+    for(let i = 0; i < 5; i++){
+      let art = document.createElement('article');
+      art.classList.add('message')
+      art.innerText = `This is article ${i+1} on the page`;
+      document.body.appendChild(art);
+    }
+  })
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
